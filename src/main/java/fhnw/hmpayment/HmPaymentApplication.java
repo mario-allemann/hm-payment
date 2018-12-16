924ce71518eea59a0d98841d057a36e48b11fe7a
phkaeppeli <32125119+phkaeppeli@users.noreply.github.com>
2018-12-16 14:25:56 +0100

    initial setup

--- src/main/java/fhnw/hmpayment/HmPaymentApplication.java
+++ src/main/java/fhnw/hmpayment/HmPaymentApplication.java
@@ -1,13 +1,40 @@
 package fhnw.hmpayment;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import fhnw.hmpayment.domain.Payment;
+import fhnw.hmpayment.domain.PaymentRepo;
 
 @SpringBootApplication
+@Controller
+@EnableAutoConfiguration
 public class HmPaymentApplication {
-
+	@Autowired
+	private PaymentRepo paymRepo;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(HmPaymentApplication.class, args);
+	}
+	
+	
+	// Map homepage
+	@RequestMapping("/")
+	@ResponseBody
+	String home() {
+		return "This is the homepage, faggots";
+	}
+
+	// Map test page
+	@RequestMapping("/payment")
+	@ResponseBody
+	Iterable<Payment> getAllItems() {
+		return paymRepo.findAll();
 	}
 
 }
